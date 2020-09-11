f2c5a478ccb753853221545a826a80b6d85c8482
DESKTOP-55NKBTR\Wei Shuang <wilyntan@gmail.com>
2020-09-11 17:53:54 +0800

    Added artifacts

--- src/main/java/circus/animal/Animal.java
+++ src/main/java/circus/animal/Animal.java
@@ -5,13 +5,9 @@ import circus.Asset;
 import java.util.Comparator;
 
 public abstract class Animal implements Asset {
-<<<<<<< HEAD
 
     public String name;
-=======
-    public String name;
 
->>>>>>> upstream/generics_cage
     public abstract String speak();
 
     public static Comparator<Animal> AnimalNameComparator = new Comparator<Animal>() {
